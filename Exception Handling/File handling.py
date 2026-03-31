0cdc633b86658f01f71728ecfe9be5c8df2592bb
Ankit15460 <111694286+Ankit15460@users.noreply.github.com>
2026-03-31 10:30:50 +0530

    Create CRUD projet

--- Exception Handling/File handling.py
+++ Exception Handling/File handling.py
@@ -1,5 +1,10 @@
 "File Handling Example to read the file"
 
-p = open(r"D:\Document\#NEPSE, #TechnicalAnalysisNepal , #.txt")
+# p = open(r"D:\Document\#NEPSE, #TechnicalAnalysisNepal , #.txt")
 
-print(p.read())
+# print(p.read())
+
+"""How to write create and overwrite"""
+r = open("Hello.txt","a")
+r.write("Jai Shree Ram")
+r.close()
